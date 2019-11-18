07732bdc3c7155afb96cd78f9a2fb9995a21597d
casterminator <44626745+casterminator@users.noreply.github.com>
2019-11-18 20:07:41 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,51 +5,52 @@ export default () =>  <div>
                           <p>Hindi mapigil ang bugso ng aking puso</p>
                           <p>Sa tuwing akoy papalapit sayo</p>
                           <p>Maaari bang hingin ang iyong kamay?</p>
-                          <p>Hawakan mot wag mong bitawan</p>
+                          <p>Hawakan mot wag mong bitawan</p><br>
 
                           <p>Hindi mapigil ang tibok ng aking puso</p>
                           <p>Sa tuwing akoy nakatingin sayo</p>
                           <p>Maaari bang huwag kang humiwalay</p>
-                          <p>Dahil sandali na lang</p>
+                          <p>Dahil sandali na lang</p><br>
 
                           <p>Darating din ang gabing walang pipigil sa 'tin</p>
-                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon?</p>
+                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon?</p><br>
 
                           <p>Kahit sandali. palayain ang pusong 'di mapigil</p>
                           <p>Sanay tayong dalawa</p>
                           <p>Sa huling pagkakataon</p>
-                          <p>Na ikay magiging akin</p>
+                          <p>Na ikay magiging akin</p><br>
 
                           <p>Hindi matigil ang gulo sa aking isip</p>
                           <p>At para bang walang kasing sakit</p>
                           <p>Alaala mong hindi ko malimutan</p>
-                          <p>Oras lang ang may alam</p>
+                          <p>Oras lang ang may alam</p><br>
 
                           <p>Kung darating din ang gabing walang pipigil sa tin</p>
-                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon</p>
+                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon</p><br>
 
                           <p>Kahit sandali, palayain ang pusong di mapigil</p>
                           <p>Sanay tayong dalwa</p>
                           <p>Sa huling pagkakataon</p>
-                          <p>Na hindi na para sa tin</p>
+                          <p>Na hindi na para sa tin</p><br>
 
                           <p>At sa bawat minuto</p>
                           <p>akoy di natuto</p>
                           <p>Ipilit mang iba</p>
-                          <p>akoy maghihintay sayo</p>
+                          <p>akoy maghihintay sayo</p><br>
 
                           <p>Ikaw ang aking kapiling sa huling sandali</p>
-                          <p>Kasalanan ba kung puso natin ang magwawagi</p>
+                          <p>Kasalanan ba kung puso natin ang magwawagi</p><br>
 
                           <p>Kahit sandali palayain ang pusong di mapigil</p>
                           <p>Sanay tayong dalwa</p>
                           <p>Sa huling pagkakataon</p>
-                          <p>Na hindi na para satin</p>
+                          <p>Na hindi na para satin</p><br>
 
                           <p>Kahit sandali patawarin ang pusong di tumigil</p>
                           <p>Para sating dalwa</p>
                           <p>Ang maling pagkakataon</p>
-                          <p>Na ikay magiging akin</p>
+                          <p>Na ikay magiging akin</p><br>
+                          
                         <h2>It is {new Date().toLocaleTimeString()}. wherever you are right now :) </h2>
                       </div>
 

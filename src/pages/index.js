b26b7fd2d43367d237ad7afd163f5575c3dca28f
casterminator <44626745+casterminator@users.noreply.github.com>
2019-11-18 19:01:36 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-export default () => <div>Hello Annie!</div>
+export default () => <div>Hello Annie! A simple website already available in the Internet. A way for me to say how much I want the whole world to know how much I care and love you and everything around you...</div>
 
-<div>A simple website already available in the Internet. A way for me to say how much I want the whole world to know how much I care and love you and everything around you...</div>
+

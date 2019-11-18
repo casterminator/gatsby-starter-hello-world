684ddc4ffb66161ad9e30883fd4f514911573ee7
casterminator <44626745+casterminator@users.noreply.github.com>
2019-11-18 19:55:17 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default () =>  <div>I LOVE YOU !!!! I LOVE YOU !!!! I LOVE YOU !!!! 
-                        <h1>Hello, world!</h1>
-                        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+                        <h1>Hello, Annie!</h1>
+                        <h2>It is {new Date().toLocaleTimeString()}. wherever you are right now :) </h2>
                       </div>
 
 

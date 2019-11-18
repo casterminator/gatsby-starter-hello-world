2b260a8289d47dec4e4f2e9e070536fc6ce6623e
casterminator <44626745+casterminator@users.noreply.github.com>
2019-11-18 20:05:19 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,55 @@
 import React from "react"
 
-export default () =>  <div>I LOVE YOU !!!! I LOVE YOU !!!! I LOVE YOU !!!! 
-                        <h1>Hello, Annie!</h1>
+export default () =>  <div> 
+                        <h1>Huling Sandali - December Avenue</h1>
+                          <p>Hindi mapigil ang bugso ng aking puso</p>
+                          <p>Sa tuwing akoy papalapit sayo</p>
+                          <p>Maaari bang hingin ang iyong kamay?</p>
+                          <p>Hawakan mot wag mong bitawan</p>
+
+                          <p>Hindi mapigil ang tibok ng aking puso</p>
+                          <p>Sa tuwing akoy nakatingin sayo</p>
+                          <p>Maaari bang huwag kang humiwalay</p>
+                          <p>Dahil sandali na lang</p>
+
+                          <p>Darating din ang gabing walang pipigil sa 'tin</p>
+                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon?</p>
+
+                          <p>Kahit sandali. palayain ang pusong 'di mapigil</p>
+                          <p>Sanay tayong dalawa</p>
+                          <p>Sa huling pagkakataon</p>
+                          <p>Na ikay magiging akin</p>
+
+                          <p>Hindi matigil ang gulo sa aking isip</p>
+                          <p>At para bang walang kasing sakit</p>
+                          <p>Alaala mong hindi ko malimutan</p>
+                          <p>Oras lang ang may alam</p>
+
+                          <p>Kung darating din ang gabing walang pipigil sa tin</p>
+                          <p>Kung hindi ngayon, aasa bang maibabalik ang kahapon</p>
+
+                          <p>Kahit sandali, palayain ang pusong di mapigil</p>
+                          <p>Sanay tayong dalwa</p>
+                          <p>Sa huling pagkakataon</p>
+                          <p>Na hindi na para sa tin</p>
+
+                          <p>At sa bawat minuto</p>
+                          <p>akoy di natuto</p>
+                          <p>Ipilit mang iba</p>
+                          <p>akoy maghihintay sayo</p>
+
+                          <p>Ikaw ang aking kapiling sa huling sandali</p>
+                          <p>Kasalanan ba kung puso natin ang magwawagi</p>
+
+                          <p>Kahit sandali palayain ang pusong di mapigil</p>
+                          <p>Sanay tayong dalwa</p>
+                          <p>Sa huling pagkakataon</p>
+                          <p>Na hindi na para satin</p>
+
+                          <p>Kahit sandali patawarin ang pusong di tumigil</p>
+                          <p>Para sating dalwa</p>
+                          <p>Ang maling pagkakataon</p>
+                          <p>Na ikay magiging akin</p>
                         <h2>It is {new Date().toLocaleTimeString()}. wherever you are right now :) </h2>
                       </div>
 
